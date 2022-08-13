da90e991d7324dc71ba685bb2062170db2284630
Vikram <viksri98@gmail.com>
2022-08-13 10:46:56 -0700

    pepflip

--- pepflip.bat
+++ pepflip.bat
@@ -1,9 +1,8 @@
 @echo off
 
-for %i in (*.png) do (
-		echo "Processing %i"
-		magick convert %i -rotate -180 r.png
-		magick convert %i r.png -smush 80 "%i.pdf"
+for %%i in (*.png) do (
+		echo "Processing %%i"
+		magick convert "%%i" -rotate -180 r.png
+		magick convert "%%i" r.png -smush 80 "%%i.pdf"
 		del r.png
-		del %i
 )
