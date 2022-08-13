0fabeb39cac8555a35a375a4de34908fd1659d1e
Vikram <viksri98@gmail.com>
2022-08-13 10:00:29 -0700

    fixed pepflip

--- pepflip.bat
+++ pepflip.bat
@@ -1,9 +1,11 @@
 @echo off
 
-for %I in (*.png) do (
+for %I in (*) do (
+	if "%~xI" == ".png" (
 		echo "Processing %I"
 		magick convert %I -rotate -180 r.png
 		magick convert %I r.png -smush 80 "%~nI.pdf"
 		del r.png
 		del %I
+	)
 )
